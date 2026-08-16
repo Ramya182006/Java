5b1a9ffa3e55729bc86b539bb1d4fc184e0414aa
Ramya <lognv59@gmail.com>
2026-08-16 21:37:48 +0530

    Left Rotation by 1 position

---
+++ src/LeftRotation.java
@@ -0,0 +1,17 @@
+public class LeftRotation {
+        public static void main(String[] args) {
+            int[] arr = {4,5,6,7,8};
+            int temp = arr[0];
+            for(int i=1;i<arr.length;i++)
+            {
+                arr[i-1]=arr[i];
+            }
+            arr[arr.length-1]=temp;
+            for(int i=0;i<arr.length;i++)
+            {
+                System.out.println(arr[i]);
+            }
+        }
+
+}
+
